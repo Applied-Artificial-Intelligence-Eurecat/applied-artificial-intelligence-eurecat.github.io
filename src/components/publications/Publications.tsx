ec6124425b5cd7f416980839fa17f8d38bfdcd02
Oriol Alàs Cercós <oriol.alas@eurecat.org>
2023-01-23 11:50:21 +0100

    Added publications FETCH API

--- src/components/publications/Publications.tsx
+++ src/components/publications/Publications.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 
 interface YearPublication {
   year: string;
@@ -6,26 +7,40 @@ interface YearPublication {
 }
 
 export function Publications() {
-  let publicationsJson: YearPublication[] = [];
-  fetch("https://raw.githubusercontent.com/Applied-Artificial-Intelligence-Eurecat/applied-artificial-intelligence-eurecat.github.io/main/src/assets/data/publications.json", {
-    "method": "GET",
-  })
-  .then(response => response.json())
-  .then(response => {
-    publicationsJson = response;
-  }).catch(err => { console.log(err); 
-  });
+
+  const [publications, setPublications] = useState<YearPublication[]>();
+
+  const fetchApi = async () => {
+    fetch(
+      "https://raw.githubusercontent.com/Applied-Artificial-Intelligence-Eurecat/applied-artificial-intelligence-eurecat.github.io/main/src/assets/data/publications.json",
+      {
+        method: "GET",
+      }
+    ).then((response) => response.json())
+      .then((response) => {
+        setPublications(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchApi()
+  }, []);
 
   return (
-    <Box style={{
-        textAlign: "justify"
-    }}>
-      {publicationsJson.map((yearMap) => (
+    <Box
+      style={{
+        textAlign: "justify",
+      }}
+    >
+      {!publications ? <div></div> : publications.map((yearMap) => (
         <div>
-            <h3>{yearMap.year}</h3>
-            {yearMap.publications.map((publication) => (
-                <p>{publication}</p>
-            ))}
+          <h3>{yearMap.year}</h3>
+          {yearMap.publications.map((publication) => (
+            <p>{publication}</p>
+          ))}
         </div>
       ))}
     </Box>
