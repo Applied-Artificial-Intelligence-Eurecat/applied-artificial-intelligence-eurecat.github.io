1b52cb26da678ea12540d25c9f54fb1183e30f65
Oriol Alàs Cercós <oriol.alas@eurecat.org>
2023-01-23 11:30:18 +0100

    Changed dynamically publications

--- src/components/publications/Publications.tsx
+++ src/components/publications/Publications.tsx
@@ -1,7 +1,21 @@
 import { Box } from "@mui/system";
-import publicationsJson from "../../assets/data/publications.json";
+
+interface YearPublication {
+  year: string;
+  publications: string[];
+}
 
 export function Publications() {
+  let publicationsJson: YearPublication[] = [];
+  fetch("https://raw.githubusercontent.com/Applied-Artificial-Intelligence-Eurecat/applied-artificial-intelligence-eurecat.github.io/main/src/assets/data/publications.json", {
+    "method": "GET",
+  })
+  .then(response => response.json())
+  .then(response => {
+    publicationsJson = response;
+  }).catch(err => { console.log(err); 
+  });
+
   return (
     <Box style={{
         textAlign: "justify"
