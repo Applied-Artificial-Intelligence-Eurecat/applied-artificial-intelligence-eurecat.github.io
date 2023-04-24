b6fe0d4fce7f082233bce8df75ef6efc5afa492b
Oriol Alàs Cercós <oriol.alas@eurecat.org>
2023-04-24 15:26:24 +0200

    Changing description body in demostrators

--- src/components/demostrators/Carrousel.tsx
+++ src/components/demostrators/Carrousel.tsx
@@ -9,6 +9,7 @@ import {
   Button,
   CardContent,
   Typography,
+  Link,
 } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
@@ -26,6 +27,7 @@ interface LineItemProp {
 interface ItemInstance {
   title: string;
   img: string;
+  link: string;
   subtitle: string;
   description: string;
   longdescription: string[];
@@ -37,15 +39,13 @@ interface CarouselWidthProps {
 
 interface DemostratorProps {
   theme: Theme;
-} 
-
-export function CarouselExample(props: DemostratorProps) {
-  return (<CarouselWidthChanger theme={props.theme}/>);
 }
 
+export function CarouselExample(props: DemostratorProps) {
+  return <CarouselWidthChanger theme={props.theme} />;
+}
 
 export function CarouselWidthChanger(props: CarouselWidthProps) {
-
   const setWindowWidth = useState(window.innerWidth)[1];
   const [divisor, setDivisor] = useState<number>(getDivider());
   const [items, setItems] = useState<ItemInstance[]>([]);
@@ -62,16 +62,14 @@ export function CarouselWidthChanger(props: CarouselWidthProps) {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       setDivisor(getDivider());
-
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up the event listener on unmount
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   return (
     <Box
@@ -88,25 +86,18 @@ export function CarouselWidthChanger(props: CarouselWidthProps) {
         swipe={false}
       >
         {groupItems(items).map((groupItem, i) => (
-          <LineItemDemostrator divisor={divisor} items={groupItem} theme={props.theme} />
+          <LineItemDemostrator
+            divisor={divisor}
+            items={groupItem}
+            theme={props.theme}
+          />
         ))}
       </Carousel>
     </Box>
   );
 }
 
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 800,
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
-
-function getDivider() : number {
+function getDivider(): number {
   var divider = 2;
   if (window.innerWidth > 1300) {
     divider = 3;
@@ -114,14 +105,13 @@ function getDivider() : number {
     divider = 2;
   } else {
     divider = 1;
-  };
+  }
   return divider;
 }
 
 function groupItems(items: ItemInstance[]) {
   const divider = getDivider();
   const mappedItems = items.reduce<ItemInstance[][]>((acc, val, index) => {
-
     const chunkIndex = Math.floor(index / divider);
 
     if (!acc[chunkIndex]) {
@@ -140,12 +130,60 @@ function CardDemostrador(props: ItemProp) {
   const handleClose = () => setOpen(false);
 
   return (
-<Paper elevation={6}
->
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {props.item.title}
-                </Typography>
+    <Paper elevation={6}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {props.item.title}
+        </Typography>
+        <Box
+          component="img"
+          alt={props.item.title}
+          src={props.item.img}
+          sx={{
+            height: "80%",
+          }}
+        />
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {props.item.subtitle}
+        </Typography>
+        <Typography variant="body2">{props.item.description}</Typography>
+      </CardContent>
+      <CardContent>
+        <Button
+          variant="contained"
+          startIcon={<KeyboardArrowRightIcon />}
+          onClick={handleOpen}
+        >
+          More information & ACCESS
+        </Button>
+      </CardContent>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: {
+              xs: "translate(-50%, -50%)",
+              lg: "translate(-50%, -70%)",
+            },
+            width: {
+              xs: "90%",
+              lg: "50%",
+            },
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Grid container>
+            <Grid item xs={6}>
+              <Typography variant="h3" component="div">
+                {props.item.title}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h3" component="div" align="right">
                 <Box
                   component="img"
                   alt={props.item.title}
@@ -154,28 +192,20 @@ function CardDemostrador(props: ItemProp) {
                     height: "80%",
                   }}
                 />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.item.subtitle}
-                </Typography>
-                <Typography variant="body2">
-                  {props.item.description}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Button
-                  variant="contained"
-                  startIcon={<KeyboardArrowRightIcon />}
-                  onClick={handleOpen}
-                >
-                  More information & ACCESS
-                </Button>
-              </CardContent>
-              <Modal open={open} onClose={handleClose}>
-                <Box sx={{ ...style }}>
-                  <h2>{props.item.title}</h2>
-                  {props.item.longdescription.map((paragraph) => (<p>{paragraph}</p>))}
-                </Box>
-              </Modal>
+              </Typography>
+            </Grid>
+            {props.item.longdescription.map((paragraph) => (
+              <p>{paragraph}</p>
+            ))}
+            <Link
+                href={props.item.link}
+                underline="hover"
+                target="_blank"
+                sx={{ marginLeft: "5px" }}
+              >You can see the demonstrator by clicking here.</Link>
+          </Grid>
+        </Box>
+      </Modal>
     </Paper>
   );
 }
@@ -190,24 +220,29 @@ function LineItemDemostrator(props: LineItemProp) {
       setDivisor(getDivider());
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up the event listener on unmount
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-      <Grid container spacing={2} justifyContent="center" sx={{
-        px: 1
-      }}>
-        {props.items.map((prop) => 
-      (<Grid item xs={Math.floor(12 / divisor)}>
-            <React.Fragment>
-       <CardDemostrador item={prop} theme={props.theme} />
-       </React.Fragment>
-       </Grid>))
-       }
-      </Grid>
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      sx={{
+        px: 1,
+      }}
+    >
+      {props.items.map((prop) => (
+        <Grid item xs={Math.floor(12 / divisor)}>
+          <React.Fragment>
+            <CardDemostrador item={prop} theme={props.theme} />
+          </React.Fragment>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
